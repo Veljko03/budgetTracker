22424526d9c565aaad07dee991508dcc2fc2c801
Veljko03 <veljkodovragovic@gmail.com>
2024-08-07 17:02:18 +0200

    finished function for adding object to db

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,3 +1,26 @@
+const incomeSchema = require("../models/incomeModel");
+
 exports.addIncome = async (request, response) => {
-  console.log(request.body);
+  const { title, amount, category, description, date } = request.body;
+
+  const income = incomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+  try {
+    if (!title || !amount || !category || !description || !date) {
+      return response.status(400).json({ message: "All fields are required!" });
+    }
+    if (amount <= 0 || !amount === "number") {
+      return response
+        .status(400)
+        .json({ message: "Amount must be possitive number!" });
+    }
+    await income.save();
+    response.status(200).json({ message: "Income Added" });
+  } catch (error) {}
+  console.log(income);
 };
