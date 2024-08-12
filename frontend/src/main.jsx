c1a32d384d3403a442cd5a28d310d5041b36d89e
Veljko03 <veljkodovragovic@gmail.com>
2024-08-12 22:13:42 +0200

    deleted unnessery folders nad components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GlobalStyle } from "./styles/GlobalStyle";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyle />
     <App />
   </StrictMode>
 );
