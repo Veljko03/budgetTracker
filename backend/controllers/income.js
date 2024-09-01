cd843f06ecd7a6e99fbdc21a8511741761fc70a8
Veljko03 <veljkodovragovic@gmail.com>
2024-09-01 21:43:38 +0200

    connected incomes id with users and added it ti incimes arr

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,16 +1,28 @@
 const incomeSchema = require("../models/incomeModel");
+const User = require("../models/usersModel");
 
 exports.addIncome = async (request, response) => {
-  const { title, amount, category, description, date } = request.body;
+  const { title, amount, category, description, date, userId } = request.body;
 
-  const income = incomeSchema({
-    title,
-    amount,
-    category,
-    description,
-    date,
-  });
   try {
+    if (!userId) {
+      return response.status(400).json({ message: "User ID is required!" });
+    }
+
+    // Pronađi korisnika pomoću userId
+    const user = await User.findById(userId);
+    if (!user) {
+      return response.status(404).json({ message: "User not found!" });
+    }
+
+    const income = incomeSchema({
+      title,
+      amount,
+      category,
+      description,
+      date,
+      user: userId,
+    });
     if (!title || !amount || !category || !description || !date) {
       return response.status(400).json({ message: "All fields are required!" });
     }
@@ -19,12 +31,13 @@ exports.addIncome = async (request, response) => {
         .status(400)
         .json({ message: "Amount must be possitive number!" });
     }
+    user.incomes = user.incomes.concat(income._id);
     await income.save();
+    await user.save();
     response.status(200).json({ message: "Income Added" });
   } catch (error) {
     response.status(500).json({ message: "Server Error" });
   }
-  console.log(income);
 };
 
 exports.getIncomes = async (request, response) => {
