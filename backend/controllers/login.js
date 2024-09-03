0a798ef6f8e566ed3ab836dc80c65899f3fd468d
Veljko03 <veljkodovragovic@gmail.com>
2024-09-03 17:40:46 +0200

    Made token expire

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -20,8 +20,9 @@ exports.loginUser = async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
-
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  });
   response
     .status(200)
     .send({ token, username: user.username, name: user.name });
